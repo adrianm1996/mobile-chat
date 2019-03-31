07d413f41cd9bd1aad11a3b744e05f697479f5a8
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-03-31 14:08:58 +0200

    commit

--- www/js/server.js
+++ www/js/server.js
@@ -2,12 +2,12 @@ var phonegap = require('connect-phonegap'),
     express = require('express'),
     path = require('path'),
     app = express(),
-    
+    http = require('http'),
     mongo = require('mongodb').MongoClient;
 
 //server.listen(process.env.PORT || 3000);
 
-var server = app.listen(3000, '::1', function () {
+var server = app.listen(process.env.PORT || 3000, '::1', function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('running at http://' + host + ':' + port)
@@ -19,36 +19,39 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', { root: '../../www' });
 });
 
-mongo.connect('mongodb://127.0.0.1/test', { useNewUrlParser: true }, function (err, db) {
-    if (err) throw err;
-    console.log("test");
-    io.sockets.on('connection', function (socket) {
-        console.log("Socket connected.");
+mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-app?retryWrites=true', { useNewUrlParser: true }, function (err, db) {
+//mongo.connect('mongodb://127.0.0.1/test', { useNewUrlParser: true }, function (err, db) {
+    if (err)
+        console.log(err);
+    else
+        io.sockets.on('connection', function (socket) {
+            console.log("Socket connected.");
 
      
-        var col = db.db().collection('messages');
+            //var col = db.db().collection('messages');
+            var col = db.db().collection('messages');
 
-        col.find().toArray(function (err, res) {
-            if (err) throw err;
-            socket.emit('output', res);
-        });
-        socket.on('message', function (msg) {
-            var whiteSpacePattern = /^\s*$/;
+            col.find().toArray(function (err, res) {
+                if (err)
+                    console.log(err);
+                else
+                    socket.emit('output', res);
+            });
+            socket.on('message', function (msg) {
+                var whiteSpacePattern = /^\s*$/;
             
-            if (whiteSpacePattern.test(msg.username) || whiteSpacePattern.test(msg.message)) {
-                socket.emit('er', "Wiadomoœæ i nazwa u¿ytkownika nie mo¿e byæ pusta.");
-                console.log("error");
-            }
-            else {
-                console.log("gut");
-                col.insert({ username: msg.username, message: msg.message })
-                io.emit('message', {
-                    message: msg.message,
-                    username: msg.username
-                });
-            }
-        });
+                if (whiteSpacePattern.test(msg.username) || whiteSpacePattern.test(msg.message)) {
+                    socket.emit('er', "Wiadomoœæ i nazwa u¿ytkownika nie mo¿e byæ pusta.");
+                }
+                else {
+                    col.insert({ username: msg.username, message: msg.message })
+                    io.emit('message', {
+                        message: msg.message,
+                        username: msg.username
+                    });
+                }
+            });
 
-    });
+        });
 
 });
