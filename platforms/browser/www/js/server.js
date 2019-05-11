4f7431024f06c3f017357e7f60dbf1b94ed787c0
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-11 13:07:25 +0200

    commit

--- platforms/browser/www/js/server.js
+++ platforms/browser/www/js/server.js
@@ -61,11 +61,7 @@ http.listen(process.env.PORT || 3000, function () {
 });
 app.use(express.static('public'));
 
-mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-app',
-    { useNewUrlParser: true },
-    function (err, db) {
-
-
+mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-app',{ useNewUrlParser: true }, function (err, db) {
         if (err) {
 
             console.log(err);
@@ -102,74 +98,71 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                         socket.emit('er', "Wpisz cos.");
                     }
                     else {
+                        app.get('/', function (req, res) {
+                            users.findOne({ user: usrLog.email }, function (err, result) {
+                                if (result == null) console.log("login invalid");
+                                else if (result.user == usrLog.email && result.passwd == usrLog.password) {
+                                    console.log("correct");
+                                    //REDIRECT
+                                    //var destination = './registration.html';
+                                    //socket.emit('redirect', destination);
+                                }
+                                else
+                                    console.log("user not found");
+                            });
 
-                        users.findOne({ user: usrLog.email }, function (err, result) {
-                            if (result == null) console.log("login invalid");
-                            else if (result.user == usrLog.email && result.passwd == usrLog.password) {
-                                console.log("correct");
-                                //REDIRECT
-                                //var destination = './registration.html';
-                                //socket.emit('redirect', destination);
-                                app.get('/', function (req, res) {
-
-                                    res.sendFile('registration.html', { root: './www' });
-                                    //res.sendFile('messages.js');
-                                });
-                            }
-                            else
-                                console.log("user not found");
                         });
+
+
                     }
+
+                    //app.post('/demo', urlencodedParser, function (req, res) {
+                    //    MongoClient.connect(url, function (err, db) {
+                    //        db.collection('userprofile').findOne({ name: req.body.name }, function (err, user) {
+                    //            if (user === null) {
+                    //                res.end("Login invalid");
+                    //            } else if (user.name === req.body.name && user.pass === req.body.pass) {
+                    //                res.render('completeprofile', { profileData: user });
+                    //            } else {
+                    //                console.log("Credentials wrong");
+                    //                res.end("Login invalid");
+                    //            }
+                    //        });
+                    //    });
+                    //});
+
+
+                    //REDIRECT
+
+                    //console.log("redirect1");
+                    //var destination = './www/registration.html';
+                    //socket.emit('redirect', destination);
+
+                    //var col = db.db().collection('messages');
+
+                    //col.find().toArray(function (err, res) {
+                    //    if (err)
+                    //        console.log(err);
+                    //    else
+                    //        socket.emit('output', res);
+                    //});
+                    //socket.on('message', function (msg) {
+                    //    var whiteSpacePattern = /^\s*$/;
+
+                    //    if (whiteSpacePattern.test(msg.username) || whiteSpacePattern.test(msg.message)) {
+                    //        socket.emit('er', "Wiadomo�� i nazwa u�ytkownika nie mo�e by� pusta.");
+                    //    }
+                    //    else {
+                    //        col.insert({ username: msg.username, message: msg.message })
+                    //        io.emit('message', {
+                    //            message: msg.message,
+                    //            username: msg.username
+                    //        });
+                    //    }
+                    //});
+
                 });
 
-
-                //app.post('/demo', urlencodedParser, function (req, res) {
-                //    MongoClient.connect(url, function (err, db) {
-                //        db.collection('userprofile').findOne({ name: req.body.name }, function (err, user) {
-                //            if (user === null) {
-                //                res.end("Login invalid");
-                //            } else if (user.name === req.body.name && user.pass === req.body.pass) {
-                //                res.render('completeprofile', { profileData: user });
-                //            } else {
-                //                console.log("Credentials wrong");
-                //                res.end("Login invalid");
-                //            }
-                //        });
-                //    });
-                //});
-
-
-                //REDIRECT
-
-                //console.log("redirect1");
-                //var destination = './www/registration.html';
-                //socket.emit('redirect', destination);
-
-                //var col = db.db().collection('messages');
-
-                //col.find().toArray(function (err, res) {
-                //    if (err)
-                //        console.log(err);
-                //    else
-                //        socket.emit('output', res);
-                //});
-                //socket.on('message', function (msg) {
-                //    var whiteSpacePattern = /^\s*$/;
-
-                //    if (whiteSpacePattern.test(msg.username) || whiteSpacePattern.test(msg.message)) {
-                //        socket.emit('er', "Wiadomo�� i nazwa u�ytkownika nie mo�e by� pusta.");
-                //    }
-                //    else {
-                //        col.insert({ username: msg.username, message: msg.message })
-                //        io.emit('message', {
-                //            message: msg.message,
-                //            username: msg.username
-                //        });
-                //    }
-                //});
-
             });
-
         }
-
-    });
+});
