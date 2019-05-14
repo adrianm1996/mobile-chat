d6b835492a548c4bc2ea23e149b43aef69cd284e
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-14 21:15:55 +0200

    login

--- platforms/android/app/src/main/assets/www/js/server.js
+++ platforms/android/app/src/main/assets/www/js/server.js
@@ -67,12 +67,12 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
 
             var col = db.db().collection('messages');
 
-            //col.find().toArray(function (err, res) {
-            //    if (err)
-            //        console.log(err);
-            //    else
-            //        socket.emit('output', res);
-            //});
+            col.find().toArray(function (err, res) {
+                if (err)
+                    console.log(err);
+                else
+                    socket.emit('output', res);
+            });
             socket.on('message', function (msg) {
                 var whiteSpacePattern = /^\s*$/;
 
@@ -81,10 +81,10 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                 }
                 else {
                     col.insert({ username: msg.username, message: msg.message })
-                    io.emit('message', {
-                        message: msg.message,
-                        username: msg.username
-                    });
+                    //io.emit('message', {
+                    //    message: msg.message,
+                    //    username: msg.username
+                    //});
                 }
             });
 
