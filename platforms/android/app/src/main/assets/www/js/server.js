781be20e61c316b84baa29086875289b9cdf5c66
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-18 12:01:33 +0200

    commit

--- platforms/android/app/src/main/assets/www/js/server.js
+++ platforms/android/app/src/main/assets/www/js/server.js
@@ -25,6 +25,38 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
         console.log(err);
     }
     else {
+
+
+        io.sockets.on('connection', function (socket) {
+            console.log("Socket connected.");
+
+
+            //var col = db.db().collection('messages');
+            var col = db.db().collection('messages');
+
+            col.find().toArray(function (err, res) {
+                if (err)
+                    console.log(err);
+                else
+                    socket.emit('output', res);
+            });
+            socket.on('message', function (msg) {
+                var whiteSpacePattern = /^\s*$/;
+
+                if (whiteSpacePattern.test(msg.username) || whiteSpacePattern.test(msg.message)) {
+                    socket.emit('er', "Wiadomo�� i nazwa u�ytkownika nie mo�e by� pusta.");
+                }
+                else {
+                    col.insert({ username: msg.username, message: msg.message })
+                    io.emit('message', {
+                        message: msg.message,
+                        username: msg.username
+                    });
+                }
+            });
+
+        });
+
         //io.sockets.on('connection', function (socket) {
         //    console.log("Socket connected.");
 
