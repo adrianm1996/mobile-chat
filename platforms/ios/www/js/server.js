fed98d0fb891104f55a89e27536cc04fdb880a40
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-18 14:44:28 +0200

    commit

--- platforms/ios/www/js/server.js
+++ platforms/ios/www/js/server.js
@@ -112,7 +112,7 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                     }
                     else {
                         col.insert({ username: msg.username, message: msg.message })
-                        io.sockets.emit('message', {
+                        io.emit('message', {
                             message: msg.message,
                             username: msg.username
                         });
