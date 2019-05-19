61a87405d97c105b6dc7ae7e56d80e240f1023f3
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-19 22:06:23 +0200

    session

--- platforms/android/app/src/main/assets/www/js/server.js
+++ platforms/android/app/src/main/assets/www/js/server.js
@@ -106,6 +106,7 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
 
                 socket.on('createChat', function (usr) {
                     var dbName = usr.loggedUsr + usr.withUser + 'CHAT';
+                    console.log(dbName);
                     db.db().collection(dbName).insert({ message: "test" });
                 });
             
