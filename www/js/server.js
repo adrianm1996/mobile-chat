23dffaa709c6fb053eb80654ec87ed29785d8427
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-19 20:35:13 +0200

    session

--- www/js/server.js
+++ www/js/server.js
@@ -152,7 +152,7 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                     }
                 });
 
-                socket.on("logout", function () {
+                socket.on("logout", function (dest) {
                     //if (socket.handshake.session.userdata) {
                     //    delete socket.handshake.session.userdata;
                     //    socket.handshake.session.save();
