9f5fa0f838608d7a14d1328501417f0a4045b0a2
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-20 22:35:22 +0200

    chat

--- platforms/ios/www/js/server.js
+++ platforms/ios/www/js/server.js
@@ -104,12 +104,11 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                     email: loggedUsr
                 });
 
+
+                var userChat;
                 socket.on('createChat', function (usr) {
                     var dbName = usr.loggedUser +"&"+ usr.withUser + 'CHAT';
-                    console.log(dbName);
-                    db.db().collection(dbName).insert({ message: "" });
-
-                    var userChat = db.db().collection(dbName);
+                    userChat = db.db().collection(dbName);
                     userChat.find().toArray(function (err, res) {
                         if (err)
                             console.log(err);
@@ -118,24 +117,24 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                             socket.emit('output', res);
                         }
                     });
-                    socket.on('message', function (msg) {
-                        var whitespacepattern = /^\s*$/;
-
-                        if (whitespacepattern.test(msg.message)) {
-                            socket.emit('er', "wiadomość nie może być pusta.");
-                        }
-                        else {
-                            userChat.insert({ username: usr.loggedUser, message: msg.message })
-                            io.of('registration.html').emit('message', {
-
-                                message: msg.message,
-                                username: usr.loggedUser
-                            });
-                        }
-
-                    });
+                    
                 });
-            
+                socket.on('message', function (msg) {
+                    var whitespacepattern = /^\s*$/;
+
+                    if (whitespacepattern.test(msg.message)) {
+                        socket.emit('er', "wiadomość nie może być pusta.");
+                    }
+                    else {
+                        userChat.insert({ username: usr.loggedUser, message: msg.message })
+                        io.of('registration.html').emit('message', {
+
+                            message: msg.message,
+                            username: usr.loggedUser
+                        });
+                    }
+
+                });
 
 
                 //var col = db.db().collection('messages');
