a88237d1ea015a21e08bb83b9d77499781c0aed0
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-20 22:09:01 +0200

    chat

--- platforms/ios/www/js/server.js
+++ platforms/ios/www/js/server.js
@@ -120,21 +120,37 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                         }
 
                     });
+                    socket.on('message', function (msg) {
+                        var whitespacepattern = /^\s*$/;
+
+                        if (whitespacepattern.test(msg.username) || whitespacepattern.test(msg.message)) {
+                            socket.emit('er', "wiadomość i nazwa użytkownika nie może być pusta.");
+                        }
+                        else {
+                            userChat.insert({ username: usr.loggedUser, message: msg.message })
+                            io.of('registration.html').emit('message', {
+
+                                message: msg.message,
+                                username: msg.username
+                            });
+                        }
+
+                    });
                 });
             
 
 
-                var col = db.db().collection('messages');
-                col.find().toArray(function (err, res) {
-                    if (err)
-                        console.log(err);
-                    else {
-                        console.log('output');
-                        socket.emit('output', res);
+                //var col = db.db().collection('messages');
+                //col.find().toArray(function (err, res) {
+                //    if (err)
+                //        console.log(err);
+                //    else {
+                //        console.log('output');
+                //        socket.emit('output', res);
 
-                    }
+                //    }
 
-                });
+                //});
                 socket.on('message', function (msg) {
                     var whitespacepattern = /^\s*$/;
 
