e2e32dacb953ee56323fd06afafe5a363124737c
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-21 21:57:35 +0200

    msg

--- www/js/server.js
+++ www/js/server.js
@@ -131,7 +131,7 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                         io.of('registration.html').emit('message', {
 
                             message: msg.message,
-                            username: usr.loggedUser
+                            username: msg.username
                         });
                     }
 
