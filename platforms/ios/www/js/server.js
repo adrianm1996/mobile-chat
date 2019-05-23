111b4cc32aeb81a13acc8a63a9ebe7d0947760bf
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-05-23 19:27:45 +0200

    commit

--- platforms/ios/www/js/server.js
+++ platforms/ios/www/js/server.js
@@ -125,7 +125,7 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                             dbName1 = usr.loggedUser + "&" + useremail + 'CHAT';
                             dbName2 = useremail + "&" + usr.loggedUser + 'CHAT';
                             var collections = db.db().listCollections();
-
+                            console.log(collections);
                             if (db.db().listCollections({ name: dbName1 }).hasNext()) {
                                 console.log('ok1');
                                 dbName = dbName1;
