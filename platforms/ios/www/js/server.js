1346608d5ebe91d13d246ad58b6059ed1b147e22
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-06-08 16:32:18 +0200

    commit

--- platforms/ios/www/js/server.js
+++ platforms/ios/www/js/server.js
@@ -223,11 +223,12 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                     else {
                         userChat.insert({ username: msg.username, message: msg.message })
 
-                        io.of('registration.html').emit('message', {
+                        socket.emit('output', res);
+                        //io.of('registration.html').emit('message', {
 
-                            message: msg.message,
-                            username: msg.username
-                        });
+                        //    message: msg.message,
+                        //    username: msg.username
+                        //});
 
 
                     }
