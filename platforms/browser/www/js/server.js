a29fe225ddfc04630722f9a7ed8cf9ffc1a1dde8
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-06-08 12:57:40 +0200

    commit

--- platforms/browser/www/js/server.js
+++ platforms/browser/www/js/server.js
@@ -1,24 +1,33 @@
 
 
 var express = require('express'),
+    session = require('express-session')({
+        secret: 'my-secret',
+        saveUninitialized: true,
+        resave: true
+    }),
     app = express(),
     http = require('http').createServer(app),
     io = require('socket.io')(http),
+    sharedsession = require("express-socket.io-session"),
     bodyParser = require('body-parser'),
     urlencodedParser = bodyParser.urlencoded({ extended: true }),
     mongo = require('mongodb').MongoClient,
     jsdom = require('jsdom'),
     JSDOM = jsdom.JSDOM,
     bcrypt = require('bcrypt-nodejs');
-    
 
 GLOBAL.document = new JSDOM('./registration.html').window.document;
 GLOBAL.window = new JSDOM('./registration.html').window;
-var $ = require('jquery');
+var direct = false;
 var loggedUsr = "loggedUser";
+var $ = require('jquery');
+var sess;
 var userChat;
 var dbName1, dbName2, dbName;
 
+
+app.use(session);
 app.use(bodyParser.json());
 app.use(urlencodedParser);
 app.use(express.static('public'));
@@ -26,6 +35,7 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', { root: './www' });
 });
 
+io.use(sharedsession(session));
 mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-app',
     { useNewUrlParser: true },
     function (err, db) {
@@ -36,14 +46,19 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
         else {
 
             io.of('/').on('connection', function (socket) {
+                console.log("Socket connected.");
+
+
 
                 var users = db.db().collection('users');
                 socket.on('user', function (usr) {
                     var whiteSpacePattern = /^\s*$/;
                     if (whiteSpacePattern.test(usr.email) || whiteSpacePattern.test(usr.password)) {
-                        socket.emit('er', "Nie podano hasła lub email.");
+                        socket.emit('er', "Wpisz cos.");
                     }
                     else {
+
+
                         var hashPassword = usr.password;
                         bcrypt.genSalt(12, function (err, salt) {
                             if (err) throw err;
@@ -76,8 +91,9 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                         socket.emit('er', "Wpisz cos.");
                     }
                     else {
+
                         users.findOne({ user: usrLog.email }, function (err, result) {
-                            if (result == null) console.log("Logowanie nie powiodło się");
+                            if (result == null) console.log("login invalid");
 
                             else
                                 bcrypt.compare(usrLog.password, result.passwd, function (errors, result2) {
@@ -85,10 +101,12 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                                         var destination = './registration.html';
                                         loggedUsr = result.user;
                                         socket.emit('redirect', destination);
+                                        direct = true;
                                     }
                                     else
-                                        console.log("nie ma takiego użytkownika");
+                                        console.log("user not found");
                                 });
+
                         });
                     }
                 });
@@ -98,21 +116,27 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
             io.of('/registration.html').on('connection', function (socket) {
                 console.log("messages connect");
 
+
                 io.of('registration.html').emit('userLogin', {
+
                     email: loggedUsr
                 });
 
+
+
                 socket.on('createChat', function (usr) {
+                    //db.getMongo().getDBNames().indexOf("mydb");
                     var userName = usr.withUserName;
                     var userSurname = usr.withUserSurname;
                     var useremail;
-
+                    console.log(userName);
+                    console.log(userSurname);
                     var findEmail = db.db().collection('users');
                     findEmail.findOne({ name: userName, surname: userSurname }, function (err, result) {
                         if (result == null) console.log("login invalid");
                         else if (result.name == userName && result.surname == userSurname) {
                             useremail = result.user;
- 
+                            console.log(useremail);
                             dbName1 = usr.loggedUser + '&' + useremail + 'CHAT';
                             dbName2 = useremail + '&' + usr.loggedUser + 'CHAT';
                             dbName = useremail + '&' + usr.loggedUser + 'CHAT';
@@ -120,12 +144,19 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                             var newDB = db.db();
                             newDB.listCollections().toArray((error, collections) => {
                                 if (collections.find(x => x.name === dbName1)) {
+                                    console.log(collections.find(x => x.name === dbName1).name);
                                     dbName = dbName1;
+                                    userChat = db.db().collection(dbName);
                                 }
-
+                                else
+                                    console.error('brak 1');
                                 if (collections.find(x => x.name === dbName2)) {
+                                    console.log(collections.find(x => x.name === dbName2).name)
                                     dbName = dbName2;
+                                    userChat = db.db().collection(dbName);
                                 }
+                                else
+                                    console.error('brak 2');
 
                                 userChat = db.db().collection(dbName);
 
@@ -139,7 +170,7 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                             });
                         }
                         else
-                            console.log("Nie ma takiego użytkownika");
+                            console.log("user not found");
                     });
                 });
 
