2887f39b9e05cc68ced676d3e651415285630005
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-06-09 16:49:04 +0200

    loading

--- platforms/browser/www/js/server.js
+++ platforms/browser/www/js/server.js
@@ -231,7 +231,7 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
 
                         console.log(socket);
 
-                        io.of('/').emit('message', {
+                        io.of('registration.html').emit('message', {
                             //socket.emit('message', {
 
                             message: msg.message,
