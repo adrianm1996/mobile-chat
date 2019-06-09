af6fa3fccdc36edae523b1c2c7c9839bd2fd4b54
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-06-09 20:51:17 +0200

    commit

--- www/js/server.js
+++ www/js/server.js
@@ -235,8 +235,15 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
 
                         //console.log(socket);
 
-                        io.of('registration.html').emit('message', {
-                        //socket.brodcast.to(socket.second).emit('message', {
+                        //io.of('registration.html').emit('message', {
+                        //    message: msg.message,
+                        //    username: msg.username,
+                        //    login: loginUser
+
+                        //});
+
+                        socket.to('connections').emit('message', {
+                            //socket.brodcast.to(socket.second).emit('message', {
 
                             message: msg.message,
                             username: msg.username,
