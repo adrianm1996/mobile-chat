db4262c5728871665208889d1e01fe7cc961a111
adrianm1996 <adrian.misiak.1996@gmail.com>
2019-06-09 19:24:54 +0200

    commit

--- platforms/browser/www/js/server.js
+++ platforms/browser/www/js/server.js
@@ -231,20 +231,20 @@ mongo.connect('mongodb+srv://admis:Turing123@cluster0-xts4d.mongodb.net/mobile-a
                         var clickuser = msg.user;
 
 
-                      
+
 
                         socket.selected.insert({ username: msg.username, message: msg.message })
 
-                            //console.log(socket);
+                        //console.log(socket);
 
-                            //io.of('registration.html').emit('message', {
-                            //    //socket.emit('message', {
+                        io.emit('message', {
+                            socket.emit('message', {
 
-                            //    message: msg.message,
-                            //    username: msg.username
-                            //});
+                                message: msg.message,
+                                username: msg.username
+                            });
 
-                        //});
+                        });
 
                     }
 
